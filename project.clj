71d5ef225e12708f95de60cc91f552910bd1f1e6
Roman Heinrich <roman.heinrich@gmail.com>
2014-01-06 19:16:07 +0100

    Squashed 'github.com/ztellman/aleph/' content from commit 3277ae3
    git-subtree-dir: github.com/ztellman/aleph
    git-subtree-split: 3277ae31fafb850e2ab14d768ae0d973a9e99d36

---
+++ project.clj
@@ -0,0 +1,31 @@
+(defproject aleph "0.3.1"
+  :description "a framework for asynchronous communication"
+  :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
+                 "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
+  :dependencies [[org.clojure/tools.logging "0.2.3"]
+                 [org.clojure/data.xml "0.0.7"]
+                 [io.netty/netty "3.6.6.Final"]
+                 [lamina "0.5.0"]
+                 [gloss "0.2.2"]
+                 [potemkin "0.2.2"]
+                 [cheshire "5.2.0"]
+                 [commons-codec/commons-codec "1.7"]
+                 [org.apache.commons/commons-compress "1.4.1"]]
+  :exclusions [org.clojure/contrib
+               org.clojure/clojure-contrib]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]
+                                  [criterium "0.3.1"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+  :aliases {"all" ["with-profile" "1.3,dev:dev:1.5,dev:1.6,dev"]}
+  :plugins [[codox "0.6.2"]]
+  :test-selectors {:default #(not (some #{:benchmark :redis} (cons (:tag %) (keys %))))
+                   :integration :redis
+                   :benchmark :benchmark
+                   :all (constantly true)}
+  :jvm-opts ["-server" "-XX:+UseConcMarkSweepGC" "-Xmx4g"]
+  :warn-on-reflection true)
