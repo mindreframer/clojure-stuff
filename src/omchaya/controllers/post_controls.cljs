d5027f606d9177e64bd8b43bfc67e3ea58df9294
Roman Heinrich <roman.heinrich@gmail.com>
2014-06-18 18:16:33 +0200

    Squashed 'github.com/sgrove/omchaya/' content from commit fe5861e
    git-subtree-dir: github.com/sgrove/omchaya
    git-subtree-split: fe5861e7f483e0f6ac2b1685dfaca2c680e36c32

---
+++ src/omchaya/controllers/post_controls.cljs
@@ -0,0 +1,155 @@
+(ns omchaya.controllers.post-controls
+  (:require [cljs.core.async :as async :refer [>! <! alts! chan sliding-buffer put! close!]]
+            [clojure.string :as string]
+            [dommy.core :as dommy]
+            [omchaya.api.mock :as api]
+            [omchaya.commands :as commands]
+            [omchaya.replay :as replay]
+            [omchaya.routes :as routes]
+            [omchaya.ui :as imp-ui]
+            [omchaya.useful :as useful :refer [ffilter]]
+            [omchaya.utils :as utils :refer [mprint]])
+  (:use-macros [dommy.macros :only [sel sel1]]))
+
+(def local-only-commands
+  ["/mute" "/unmute"])
+
+(defn sendable-message? [message]
+  (let [[command & _] (string/split message #" |\n")]
+    (if (or (some #{command} local-only-commands)
+            (empty? message))
+      false
+      true)))
+
+(defmulti post-control-event!
+  (fn [target message args previous-state current-state] message))
+
+(defmethod post-control-event! :default
+  [target message args previous-state current-state]
+  (mprint "No post-control for: " message))
+
+(defmethod post-control-event! :current-user-mentioned
+  [target message args previous-state current-state]
+  (mprint "notify current user they were mentioned"))
+
+(defmethod post-control-event! :playlist-entry-played
+  [target message [order channel-id] previous-state current-state]
+  (let [controls-ch (get-in current-state [:comms :controls])
+        playlist (get-in current-state [:channels channel-id :player :playlist])
+        entry (ffilter #(= (:order %) order) (get-in current-state [:channels channel-id :player :playlist]))]
+    (put! controls-ch [:audio-player-source-updated [(:src entry) channel-id]])))
+
+(defmethod post-control-event! :audio-player-source-updated
+  [target message [src channel-id] previous-state current-state]
+  (when (and (= channel-id (:selected-channel current-state))
+             (= (get-in current-state [:channels (:selected-channel current-state) :player :state])
+                :playing))
+    (let [player (sel1 target [(str ".audio-player.audio-" channel-id)])]
+      (js/setTimeout #(.play player) 35))))
+
+(defmethod post-control-event! :tab-selected
+  [target message channel-id previous-state current-state]
+  (utils/set-window-href! (routes/v1-channel-link {:channel-id channel-id}))
+  (when-let [new-channel (get-in current-state [:channels channel-id])]
+    (js/setTimeout #(imp-ui/scroll-to-latest-message! target (:id new-channel)) 35)))
+
+(defmethod post-control-event! :user-message-submitted
+  [target message args previous-state current-state]
+  (let [channel (get-in current-state [:channels (:selected-channel current-state)])
+        user-message (get-in previous-state [:settings :forms :user-message :value])
+        content    (get-in previous-state [:settings :forms :user-message :value])
+        user       (get-in current-state [:users (:current-user-email current-state)])
+        channel    (get-in current-state [:channels (:selected-channel current-state)])
+        api-key    (:api-key user)
+        activity   {:content    content
+                    :author     (:email user)
+                    :created_at (js/Date.)
+                    :channel-id (:id channel)}]
+    (when-let [input (sel1 target [:.chat-input])]
+      (dommy/set-value! input ""))
+    (js/setTimeout #(imp-ui/scroll-to-latest-message-when-appropriate! target (:id channel)) 35)
+    (commands/handle-maybe-command target {:content user-message
+                                           :channel-id (:id channel)} current-state)
+    (when (sendable-message? user-message)
+      (api/send-user-message! api-key activity))))
+
+(defmethod post-control-event! :audio-player-started
+  [target message channel-id previous-state current-state]
+  (let [player (sel1 target [(str ".audio-player.audio-" channel-id)])]
+    (js/setTimeout #(.play player) 35)))
+
+(defmethod post-control-event! :audio-player-stopped
+  [target message channel-id previous-state current-state]
+  (let [player (sel1 target [(str ".audio-player.audio-" channel-id)])]
+    (js/setTimeout #(.pause player) 35)))
+
+(defmethod post-control-event! :audio-player-muted
+  [target message args previous-state current-state]
+  (let [players (sel target :.audio-player)]
+    (js/setTimeout #(doseq [player players]
+                      (set! (.-muted player) true)) 35)))
+
+(defmethod post-control-event! :audio-player-unmuted
+  [target message args previous-state current-state]
+  (let [players (sel target :.audio-player)]
+    (js/setTimeout #(doseq [player players]
+                      (set! (.-muted player) false)) 35)))
+
+(defmethod post-control-event! :current-user-mentioned
+  [target message [activity url] previous-state current-state]
+  (let [player (sel1 target [(str ".audio-player.sfx.audio-" (:channel-id activity))])]
+    (js/setTimeout #(.play player) 35)))
+
+(defmethod post-control-event! :user-logged-out
+  [target message [activity url] previous-state current-state]
+  (mprint "Log the user out somehow"))
+
+(defmethod post-control-event! :history-player-opened
+  [target message [activity url] previous-state current-state]
+  (when-let [player-el (sel1 :div#player-container)]
+    (replay/install-player! player-el (get-in current-state [:users (:current-user-email current-state) :api-key])
+                            (replay/initial-player-state (:comms current-state)
+                                                         {:player-control replay/player-control-ch
+                                                          :player-drag    replay/player-drag-ch
+                                                          :api replay/api-ch}))))
+
+(defmethod post-control-event! :search-field-focused
+  [target message [activity url] previous-state current-state]
+  (when-let [search-field (sel1 [target :input.query])]
+    ;; Really unpleasant, but handles the bug where the input field is rendered blank when re-focused
+    (.setTimeout js/window
+                 #(set! (.-value search-field) (get-in current-state [:settings :forms :search :value]))
+                 20)))
+
+(defmethod post-control-event! :search-focus-key-pressed
+  [target message args previous-state current-state]
+  (when-let [search-field (sel1 [target :input.query])]
+    (.focus search-field)))
+
+(defmethod post-control-event! :search-form-blur-key-pressed
+  [target message args previous-state current-state]
+  (when-let [message-field (sel1 [target :textarea.chat-input])]
+    (.focus message-field)))
+
+(defmethod post-control-event! :user-message-blur-key-pressed
+  [target message args previous-state current-state]
+  (when-let [search-field (sel1 [target :input.query])]
+    (.focus search-field)))
+
+(defmethod post-control-event! :channel-destroyed
+  [target message channel-id previous-state current-state]
+  (api/destroy-channel! (get-in current-state [:comms :api]) channel-id))
+
+(defmethod post-control-event! :state-restored
+  [target message channel-id previous-state current-state]
+  (when (empty? (.getItem js/localStorage "omchaya-state"))
+    (print "No data available to load from localStorage")))
+
+(defmethod post-control-event! :state-persisted
+  [target message channel-id previous-state current-state]
+  (.setItem js/localStorage "omchaya-state" (pr-str (dissoc current-state :comms))))
+
+(defmethod post-control-event! :window-resized
+  [target message channel-id previous-state current-state]
+  ;; Figure out re-layout code here
+  )
---
+++ src/omchaya/utils.cljs
@@ -0,0 +1,80 @@
+(ns omchaya.utils
+  (:require [cljs.core.async :as async :refer [>! <! alts! chan sliding-buffer put! close!]]
+            [clojure.string :as string]
+            [goog.crypt :as crypt]
+            [goog.crypt.Md5]
+            [goog.events :as ge]
+            [goog.Uri]
+            [goog.net.EventType :as gevt]
+            [goog.i18n.NumberFormat.Format :as formats]
+            [dommy.core :as dommy])
+  (:require-macros [cljs.core.async.macros :as am :refer [go alt!]])
+  (:import [goog.net XhrIo]
+           [goog.async Deferred])
+  (:use-macros [dommy.macros :only [node sel sel1]]))
+
+(defn uuid
+  "returns a type 4 random UUID: xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
+  []
+  (let [r (repeatedly 30 (fn [] (.toString (rand-int 16) 16)))]
+    (apply str (concat (take 8 r) ["-"]
+                       (take 4 (drop 8 r)) ["-4"]
+                       (take 3 (drop 12 r)) ["-"]
+                       [(.toString  (bit-or 0x8 (bit-and 0x3 (rand-int 15))) 16)]
+                       (take 3 (drop 15 r)) ["-"]
+                       (take 12 (drop 18 r))))))
+
+(def parsed-uri
+  (goog.Uri. (-> (.-location js/window) (.-href))))
+
+(def initial-query-map
+  {:kandan-channels  (string/split (or (.getParameterValue parsed-uri "kandan-channels") "1") #",")
+   :kandan-api-key   (.getParameterValue parsed-uri "kandan-api-key")
+   :kandan-client?   (seq (.getParameterValue parsed-uri "kandan-api-key"))
+   :log-channels?    (or (.getParameterValue parsed-uri "log-channels") false)
+   :logging-enabled? (= (.getParameterValue parsed-uri "logging-enabled") "true")
+   :restore-state?   (= (.getParameterValue parsed-uri "restore-state") "true")})
+
+(def logging-enabled?
+  (:logging-enabled? initial-query-map))
+
+(defn mprint [& message]
+  (when logging-enabled?
+    (apply print message)))
+
+(defn safe-sel [s]
+  (str (string/replace (string/lower-case (str s)) #"[\W]" "-")))
+
+(defn email->gravatar-url [email]
+  (let [email (or email "unknown-email@unknown-domain.com")
+        container (doto (goog.crypt.Md5.)
+                    (.update email))
+        hash (crypt/byteArrayToHex (.digest container))]
+    (str "http://gravatar.com/avatar/" hash "?s=30&d=identicon")))
+
+(defn gravatar-for [email]
+  [:img.avatar
+   {:src
+    (email->gravatar-url email)}])
+
+(defn set-window-href! [path]
+  (js/window.history.pushState #js {}, "", path))
+
+(defn ajax [url method data-string success & [error headers]]
+  (let [request (XhrIo.)
+        d (goog.async.Deferred.)
+        listener-id (ge/listen request gevt/COMPLETE (fn [response]
+                                                       (let [target (.-target response)
+                                                             data (if (= method "DELETE")
+                                                                    nil
+                                                                    (.getResponseJson target ))]
+                                                         (.callback d data))))]
+                                        ; Setup deferred callbacks
+    (.addErrback d  (fn [error] (.log js/console "Error on ajax: " error)))
+    (when success (.addCallback d #(success (js->clj % :keywordize-keys true))))
+    (when error (.addErrback d error))
+    (.addBoth d (fn [value] (ge/unlistenByKey listener-id) (.dispose request)))
+    (mprint (str "Firing request to " url " via " method " and data - : " data-string))
+                                        ; Fire request
+    (.send request url method data-string headers)
+    request))
